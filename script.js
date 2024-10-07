ebb8a7566993a1adabf0ef8ed6ecfbd1619ac520
emmeiwhite <emmeiwhite@gmail.com>
2024-10-07 15:54:48 +0530

    Successfully moving forward with Promise Handling

--- script.js
+++ script.js
@@ -220,10 +220,36 @@ const imgContainer = document.querySelector('.img-container')
 const btn = document.querySelector('button')
 
 btn.addEventListener('click', () => {
-  const promise = loadImage(url)
-  console.log(promise)
+  // const promise = loadImage(url)
+  // console.log(promise)
+
+  loadImage(url)
+    .then(data => console.log(data))
+    .catch(error => console.log('Error Fetching Data'))
 })
 
 function loadImage(url) {
-  return new Promise((resolve, reject) => {})
+  return new Promise((resolve, reject) => {
+    let img = new Image()
+
+    /* --- We can add event listeners for the img
+    a) load: if we are able to load the image
+    b) error: if we are unable to load the image
+    TRICK: we can use img.src = url to direct the img tag to its source
+            - If the url source points to a correct url -> image is loaded
+            - If the url source points to an incorrect url -> image is not loaded
+    --- */
+
+    img.addEventListener('load', () => {
+      console.log('Successfully loaded the image !')
+    })
+
+    img.addEventListener('error', () => {
+      console.log('Failed to load the image')
+    })
+
+    img.src = url // Here lies all the trick.
+    /** If src points to a correct resource the 'load' event is fired!
+     * If src points to an incorrect resource the 'error' event is fired! **/
+  })
 }
